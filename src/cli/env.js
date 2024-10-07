a3a6f5fc5e386ccfbcc54b986b1a0ec3c1364f1c
Mikhail <michael@roofbuddy.co.nz>
2024-10-08 00:29:26 +0300

    :feat: implement cli subtask

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+	const envVars = env;
+	const rssVars = Object.entries(envVars)
+		.filter(([key]) => key.startsWith('RSS_'))
+		.map(([key, value]) => `${key}=${value}`)
+		.join('; ');
+
+	console.log(rssVars);
 };
 
 parseEnv();
