f636a504f095cf1aec43b185dbae38e33bfeb4fe
Mikhail <michael@roofbuddy.co.nz>
2025-04-28 21:15:27 +0300

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const fileToRemove = path.join(
+	new URL('.', import.meta.url).pathname,
+	'files',
+	'fileToRemove.txt'
+);
 const remove = async () => {
-    // Write your code here 
+	try {
+		await fs.access(fileToRemove);
+		throw new Error('FS operation failed');
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			console.log('File does not exist.');
+			return;
+		}
+	}
+
+	await fs.unlink(fileToRemove);
+	console.log('File removed successfully!');
 };
 
 await remove();
