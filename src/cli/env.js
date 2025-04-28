4f8601198c24f693aa3c3ef79798b70df90fa88c
Mikhail <michael@roofbuddy.co.nz>
2025-04-28 21:45:00 +0300

    feat: implement cli env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,21 @@
 const parseEnv = () => {
-    // Write your code here 
+	const envVariables = [];
+
+	for (const [key, value] of Object.entries(process.env)) {
+		if (key.startsWith('RSS_')) {
+			envVariables.push({
+				key: key,
+				value: value,
+			});
+		}
+	}
+
+	const resultToPrint = envVariables
+		.map((env) => {
+			return `${env.key}=${env.value}`;
+		})
+		.join('; ');
+	console.log(resultToPrint);
 };
 
 parseEnv();
