fc5c2f77f1561122ff09381fa1788a33bf57738a
Mikhail <michael@roofbuddy.co.nz>
2025-04-28 22:35:50 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, './files/fileToCalculateHashFor.txt');
+const algorithm = 'sha256';
+
 const calculateHash = async () => {
-    // Write your code here 
+	const hash = createHash(algorithm);
+	const fileStream = createReadStream(filePath);
+
+	await pipeline(fileStream, hash);
+	const hashValue = hash.digest('hex');
+	console.log(hashValue);
 };
 
 await calculateHash();
