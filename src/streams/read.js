078e2de897d158c9bd288ec050a5ff625d5c5413
Mikhail <michael@roofbuddy.co.nz>
2025-04-28 22:45:47 +0300

    feat: implement stream read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { finished } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, './files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+	const fileStream = createReadStream(filePath);
+
+	fileStream.pipe(process.stdout);
+
+	await finished(fileStream);
+	console.log('\nFile read successfully.');
 };
 
 await read();
