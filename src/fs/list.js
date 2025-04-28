7d822e48c9335129149e6765daefce27c35beea6
Mikhail <michael@roofbuddy.co.nz>
2025-04-28 21:27:46 +0300

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const sourceDirectory = path.join(
+	new URL('.', import.meta.url).pathname,
+	'files'
+);
+
 const list = async () => {
-    // Write your code here 
+	try {
+		await fs.access(sourceDirectory);
+		throw new Error('FS operation failed');
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			console.log('Source directory does not exist.');
+			return;
+		}
+	}
+	const files = await fs.readdir(sourceDirectory);
+    const title = files.length === 1 ? 'File' : 'Files';
+	if (files.length === 0) {
+		console.log('Directory is empty.');
+		return;
+	}
+	console.log(`${files.length} ${title} in the ${sourceDirectory} directory:`, files);
 };
 
 await list();
